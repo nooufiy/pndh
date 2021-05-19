1ca6aa369c5285c48eacceec673cf6d0a9f3e31d
nou <83199729+nooufiy@users.noreply.github.com>
2021-05-20 01:17:23 +0700

    option srv type

--- run.sh
+++ run.sh
@@ -2,9 +2,36 @@
 
 # bash run.sh fat 123.45.67.89
 
-svt=$1				# server type (fat or shu)
+svt=$1				# server type (ilm ,fat or shu)
 svr=$2				# server ip
 
-bash ilamp-p72-simple.sh
-bash pindah-"$svt".sh "$svr"
+[ -z "$svt" ] && echo "set server type" && exit 0 
 
+yum install git -y
+yum install dos2unix -y
+git clone https://github.com/nooufiy/ilamp72.git
+
+if [ "$svt" == "ilm" ]; then 
+  cd ilamp72
+  chmod +x *.sh
+  dos2unix *.sh
+  
+  sh ins.sh
+
+else
+  [ -z "$svr" ] && echo "set server ip" && exit 0 
+  git clone https://github.com/nooufiy/pndh.git
+  
+  cd ilamp72
+  chmod +x *.sh
+  dos2unix *.sh
+  
+  sh ins.sh
+  
+  cd ../pndh
+  chmod +x *.sh
+  dos2unix *.sh
+  
+  sh pindah-"$svt".sh "$svr"
+
+fi
